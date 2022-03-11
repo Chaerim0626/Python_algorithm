39be28c2964ab673abc7d75b50dcee75356d0016
Chaerim Park <dasapcr@gmail.com>
2022-03-11 17:58:16 +0900

    0311 commit 3

--- basic_grammer/main.py
+++ basic_grammer/main.py
@@ -211,6 +211,35 @@ for x in a:
         print(y, end=' ') #리스트 출력
 
 #함수 만들기
+def add(a, b):
+    c = a+b
+    print(c)
+add(3, 2)
+
+def add(a, b):
+    c = a + b
+    d = a - b
+    return c, d
+
+print(add(3, 2))
+
+def isPrime(x):
+    for i in range(2, x):
+        if x%i == 0:
+            return False
+        else:
+            return True
+
+
+a = [12, 13, 7, 9, 19]
+for y in a:
+    if isPrime(y)
+        print(y, end=' ')
+
+
+#람다 함수
+
+
 
 
 
