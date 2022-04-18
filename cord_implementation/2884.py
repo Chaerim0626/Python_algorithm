e6b0c0bee89c7e52bf0c5b50f2081fcbc3476842
Chaerim Park <dasapcr@gmail.com>
2022-04-19 00:00:27 +0900

    0419 upload

--- cord_implementation/2884.py
+++ cord_implementation/2884.py
@@ -1,4 +1,8 @@
 h, m = map(int, input().split())
 if m >= 45:
-    print("{} : {}".format(h,m-45))
-elif
+    print(h, m-45)
+elif h == 0: #0시고 분이 45분 보다 작은 경우
+    h = 23
+    print(h,60-(45-m))
+else: # 0시도 아니고 45분보다 작은경우
+    print(h-1, 60-(45-m))
