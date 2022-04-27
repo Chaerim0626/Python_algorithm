cf765a5d63949e2c60017b13e02d8a9133cc4359
Chaerim Park <dasapcr@gmail.com>
2022-04-27 17:04:24 +0900

    0427 upload2

---
+++ cord_implementation/DataStructure/10799.py
@@ -0,0 +1,14 @@
+s = input()
+cnt = 0
+stack = []
+for i in range(len(s)):
+    if s[i] == ')':
+        if s[i-1] == ')':
+            stack.pop()
+            cnt += 1
+        else:
+            stack.pop()
+            cnt += len(stack)
+    else:
+        stack.append(i)
+print(cnt)
